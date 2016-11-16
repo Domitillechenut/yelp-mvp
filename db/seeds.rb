60d5cba9bdf2a28dd36395d9fec5d6a94ba64e21
Domitille Chenut <domitille.de.percin@gmail.com>
2016-11-16 14:46:31 +0100

    seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
+
+require 'faker'
+
+# Creating 100 fake restaurants
+20.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save!
+end
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
