89c0b73192aaba8d8dde24f5ced232ee2e910dc8
Domitille Chenut <domitille.de.percin@gmail.com>
2016-11-16 13:08:25 +0100

    add model review&restau

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+    redirect_to review_path(@review)
+  end
+
+end
+end
