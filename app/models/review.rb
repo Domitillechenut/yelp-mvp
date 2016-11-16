ff6323d9e7206262de19d3fd1f26f601007927a9
Domitille Chenut <domitille.de.percin@gmail.com>
2016-11-16 12:57:17 +0100

    maj blank

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,14 +1,9 @@
 class Review < ApplicationRecord
   belongs_to:restaurant
 
-  validates :content, presence: true
-  validates :rating, inclusion: {in:[0, 1, 2, 3, 4, 5], allow_nil: false}, numericality: { only_integer: true }
+  validates :content, presence: true, allow_blank: false
+  validates :rating, inclusion: {in:[0, 1, 2, 3, 4, 5], allow_nil: false}, numericality: { only_integer: true }, allow_blank: false
 end
 
-# class Review < ApplicationRecord
-#   belongs_to:restaurant
 
-#   validates :content, presence: true
-#   validates :rating, inclusion: {in:[0, 1, 2, 3, 4, 5], allow_nil: false}, numericality: { only_integer: true }
-# end
 
